cafe53f6f587e4d6ef36aa53fc9b4fda8e2da038
Darvisechavarria <yerikechavarria210@gmail.com>
2022-03-15 15:46:48 -0500

    Taller2-AVANZADA taller #2 evaluativo terminado

--- punto3.js
+++ punto3.js
@@ -6,21 +6,21 @@ let alimentos =[
     {nombre:"lechuga",tipo:"vegetal",niveldeEnergia:300},
     {nombre:"zanahoria",tipo:"vegetal",niveldeEnergia:300},
     {nombre:"mosca",tipo:"insecto",niveldeEnergia:150},
-    {nombre:"lagarto",tipo:"animal",niveldeEnergia:300}
+    {nombre:"lagarto",tipo:"animal",niveldeEnergia:300},
+    {nombre:"banano",tipo:"vegetal",niveldeEnergia:400}
 ]
-
 function recibirAlimentos(alimentos,callback){
-    setTimeout(function(){
-        let alimento={}
-        let alimentosFiltrados= alimento.filter(function(alimento){
-            return(alimento.vegetal && alimento.niveldeEnergia >=200)
+    setTimeout(function(){        
+        let alimentosFiltrados = alimentos.filter(function(alimento){
+            return(alimento.tipo =='vegetal' && alimento.niveldeEnergia >=200)
         })
         //llamo al callback
         callback(alimentosFiltrados)
-    },1000)
+    },5000)
 }
 //LLAMADO A LA FUNCION PRINCIPAL
 recibirAlimentos(alimentos,function(alimentosFiltrados){
-    sumaNiveles=alimentosFiltrados.niveldeEnergia
-    console.log("la suma de niveles es: " + sumaNiveles)
+    let sumaNiveles = alimentosFiltrados.map(alimento => alimento.niveldeEnergia).reduce((prev, curr) => prev + curr, 0)
+    console.log(alimentosFiltrados)
+    console.log("la suma de niveles de los vegetales de nivel mayor a 200 es: " + sumaNiveles)
 })
